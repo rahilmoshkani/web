846d305b9912ae2f5c5e86b7a8219e5a1a0065b9
rahil moshkani <rahilmoshkani@gmail.com>
2021-12-28 16:52:55 +0330

    routerlink firstpage & about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { FirstpageComponent } from './firstpage/firstpage.component';
+import { AboutComponent } from './about/about.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'firstpage',component:FirstpageComponent},
+  {path:'About',component:AboutComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
