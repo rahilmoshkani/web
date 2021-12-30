9d7c16b5278957ac369b614bedf3496318f2f3a0
rahil moshkani <rahilmoshkani@gmail.com>
2021-12-30 17:16:21 +0330

    portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
-  constructor() { }
+
+
+  constructor() {
+     }
 
   ngOnInit(): void {
+
+
   }
 
 }
