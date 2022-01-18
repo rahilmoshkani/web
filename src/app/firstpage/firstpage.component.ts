044ec99715074cd11e9a52bab3e6481f26c265e2
rahil moshkani <rahilmoshkani@gmail.com>
2022-01-18 20:26:23 +0330

    css

--- src/app/firstpage/firstpage.component.ts
+++ src/app/firstpage/firstpage.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-firstpage',
@@ -7,11 +9,38 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./firstpage.component.scss','./firstpage.en.css','./firstpage.fa.css']
 })
 export class FirstpageComponent implements OnInit {
-  constructor(public translate:TranslateService){
+  constructor(public translate:TranslateService,@Inject(Document) private document:Document){
     translate.addLangs(['en','fa']);
     translate.setDefaultLang('en');
     const browserLang = translate.getBrowserLang();
     translate.use(browserLang.match(/en|fa/)? browserLang:'en');
+  }
+  changeLangage(lang:string){
+    let htmlTag=this.document.getElementsByName('html')[0]as HTMLHtmlElement;
+    htmlTag.dir=lang==='fa'?'rtl':'ltr';
+    this.translate.setDefaultLang(lang);
+    this.translate.use(lang);
+    this.changeCssFile(lang);
+
+  }
+  changeCssFile(lang:string){
+    let headTag=this.document.getElementsByTagName('head')[0] as HTMLHtmlElement;
+
+    let existingLink=this.document.getElementById('langCss') as HTMLLinkElement;
+
+    let bundleName=lang==="fa"?
+    'farsiStyle.css':'englishStyle.css';
+
+    if(existingLink){
+      existingLink.href=bundleName;
+    }else{
+      let newLink=this.document.createElement('link');
+      newLink.rel='stylesheet';
+      newLink.type='text/css';
+      newLink.id='langCss';
+      newLink.href=bundleName;
+      headTag.appendChild(newLink);
+    }
   }
 
 
